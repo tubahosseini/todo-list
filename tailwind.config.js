d6a601c7618388794fc19b2aec4f2899a7f21791
Tuba Hosseini <tubahosseini@gmail.com>
2024-03-16 12:13:31 +0330

    header generated

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'hw-dark-purple': '#6200ea',
+        'hw-light-purple': '#7926ed'
+      },
+    },
+  },
+  plugins: [],
+}
+
